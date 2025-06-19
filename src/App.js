c3bd1b5575aca1ca99f167e0827740041e4bd08e
Rkaklij23 <123876745+Rkaklij23@users.noreply.github.com>
2025-06-19 23:45:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
-import { IoMenu, IoLogoGithub } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
 import ProfilePhoto from "./images/rutuja.jpg";
 import {
   VerticalTimeline,
